fef1197d192461e2b4ed4bbfb5fbcd0e53000248
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-29 19:40:18 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,9 +9,15 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    @student = Student.create({
+        first_name: params[:first_name],
+        last_name: params[:last_name]
+      })
+    redirect_to @student
   end
 
 end
