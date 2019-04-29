d9a4e675673149d9629e90cb3d83f925c0f80576
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-29 19:39:17 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,7 +13,10 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.create(params)
+    @student = Student.create({
+        first_name: params[:first_name],
+        last_name: params[:last_name]
+      })
     redirect_to @student
   end
 
